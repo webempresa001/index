64a6c7084217bfbee9630858c119d7db3cfeda48
webempresa001 <70814512+webempresa001@users.noreply.github.com>
2020-09-08 22:00:25 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,7 +119,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
     dots: true,
     loop: true,
     items: 1
