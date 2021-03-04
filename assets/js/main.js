2d188bce0b1857a1d1c42acd24f24574fbb0d9ee
manuelmsni <manuelmsni@hotmail.com>
2021-03-04 02:01:38 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,6 +265,32 @@ function BgSet(ElementId, ThisElementURL)
   } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
+  
+    // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, .mobile-nav');
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop() + 200;
+
+    nav_sections.each(function() {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  
+  
   // Toggle .header-scrolled class to #header when page is scrolled 
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
