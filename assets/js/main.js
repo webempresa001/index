cf982b8d360780bdc80745c213190ac07c08e3da
manuelmsni <manuelmsni@hotmail.com>
2021-03-04 01:02:04 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,25 +269,21 @@ function BgSet(ElementId, ThisElementURL)
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
-
-  if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-  }
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
       $('.premio2019').addClass('premio-scrolled');
     } else {
+      $('#header').removeClass('header-scrolled');
       $('.back-to-top').fadeOut('slow');
       $('.premio2019').removeClass('premio-scrolled');
     }
   });
 
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+    $('.back-to-top').fadeIn('slow');
+    $('.premio2019').addClass('premio-scrolled');
+  }
+  // Back to top button
   $('.back-to-top').click(function() {
     $('html, body').animate({
       scrollTop: 0
